32a2e9da0473cd7071374f038b95f0d45cd65e68
Tom <tomdakan@gmail.com>
2025-10-19 21:23:33 -0700

    style: corrected formatting in a couple of files

--- template/{{ project_slug }}/src/{{ module_name }}/settings.py
+++ template/{{ project_slug }}/src/{{ module_name }}/settings.py
@@ -1,7 +1,5 @@
-from typing import Optional
-
 from pydantic import SecretStr
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 try:
     from dotenv import load_dotenv  # type: ignore
@@ -15,13 +13,22 @@ if _DOTENV_AVAILABLE:
 
 
 class Settings(BaseSettings):
+    """
+    Settings for the application.
+
+    Reads settings from environment variables.
+    You can create a .env file in the root of the project to store
+    sensitive information.
+    """
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+
     app_name: str = "{{ project_name }}"
     debug: bool = True
-    secret_key: Optional[SecretStr] = None
-    redis_dsn: Optional[str] = None
-
-    class Config:
-        pass
+    # The Optional[X] syntax has been replaced with X | None
+    secret_key: SecretStr | None = None
+    redis_dsn: str | None = None
 
 
+# Instantiate the settings
 settings = Settings()
